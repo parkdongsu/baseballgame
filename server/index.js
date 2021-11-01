e712ad7ffe325dae353187da5ba62c738a2c0b85
parkdongsu <dongsu2005@naver.com>
2021-11-01 11:37:14 +0900

    나가기 버튼 추가, 채팅창 스크롤 다운 추가

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const cors = require('cors')
+const cors = require('cors');
+const e = require('express');
 const io = require('socket.io')(server,{
     cors : {
         origin :"*",
@@ -8,14 +9,26 @@ const io = require('socket.io')(server,{
     }
 });
 
+let roomList = []
+
+
 io.on('connection', (socket) => {
     socket.on('join',({username, room}) =>{
         socket.join(room)
         console.log(username, '가' , room, '번 방에 입장하셨습니다')
-        io.to(room).emit('adminMessage',{username,room})
-
+        enterRoom(username,room)
+        console.log(roomList)
+        io.to(room).emit('joinRoom',{username,room})
     })
 
+    socket.on('leave',({username, room}) =>{
+        socket.leave(room)
+        console.log(username, '가' , room, '번 방에서 퇴장하셨습니다.')
+        exitRoom(username,room)
+        console.log(roomList)
+        io.to(room).emit('leaveRoom',{username,room})
+    })
+    
     socket.on('message',({username,room,message}) => {
         console.log(username, ':' , message)
         io.to(room).emit('message',({username, message}))
@@ -37,6 +50,25 @@ io.on('connection', (socket) => {
     })
     
 })
+
+const enterRoom = (username,room) =>{
+    let roomInfo = roomList.find(element => element.room == room)
+    if( roomInfo == undefined){
+        roomList.push({'room': room, 'users' : [username]})
+    }else if(roomInfo.users.includes(username) === false){
+        roomInfo.users.push(username)
+    }
+}
+
+const exitRoom = (username,room) =>{
+    let userInfo = roomList.find(element => element.room == room).users
+    for(let i = 0; i< userInfo.length; i++){
+        if(userInfo[i] === username){
+            userInfo.splice(i,1);
+            i--;
+        }
+    }
+}
 
 server.listen(4000, function(){
     console.log('listening on port 4000');
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import io from 'socket.io-client';
-import TextField from '@material-ui/core/TextField';
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button';
 import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
 
 const socket =  io.connect('http://localhost:4000')
 
@@ -13,6 +13,12 @@ const Main = () => {
     const room = window.location.href.split("room/")[1];
     const [chat,setChat] = useState([]);
     const [message,setMessage] = useState('');
+    const scrollRef = useRef();
+
+    useEffect(() =>{
+        scrollRef.current.scrollIntoView({ behavior: 'auto', block: 'end', inline: 'nearest' });
+    },[chat])
+
 
     useEffect(() =>{
         socket.emit('join', {username,room}, (error) =>{
@@ -21,8 +27,11 @@ const Main = () => {
             }
         })
 
-        socket.on('adminMessage',({username,room}) =>{
+        socket.on('joinRoom',({username,room}) =>{
             setChat(oldChat => [...oldChat,`${username} 님 ${room} 번 방에 오신 것을 환영합니다.`])
+        })
+        socket.on('leaveRoom',({username,room}) =>{
+            setChat(oldChat => [...oldChat,`${username} 님이 ${room} 번 방에서 퇴장하셨습니다.`])
         })
         socket.on('message',({username, room, message}) =>{
             setChat(oldChat => [...oldChat,`${username} : ${message}`])
@@ -53,20 +62,38 @@ const Main = () => {
         })
     }
 
+    const onClickexit = () =>{
+        socket.emit('leave', {username,room}, (error) =>{
+            if(error){
+                alert('error!!')
+            }
+        })
+        
+    }
+
+
+
 
     return (
         <Grid container spacing={2}>
             <Grid item xs={6} md={8}>
             </Grid>
             <Grid direction="column" item xs={6} md={4}>
-                <div>game chat</div>
-                <Grid style={{height:'200px', overflowY: 'auto', border: '1px solid black', background: '#b2c7d9'}}item xs={4}>
+                <Grid item xs={4}>
+                    <Exit>
+                    <LinkStyle to={`/`} onClick={onClickexit}>나가기</LinkStyle>
+                    </Exit>
+                </Grid>
+                <Grid style={chatStyle} item xs={4}>
+                    <div ref={scrollRef}>
                     {renderChat()}
+                    </div>
+                    
                 </Grid>
                 <Grid item xs={4}>
                     <form onSubmit={onSubmitMessage}>
-                        <input name='message' value={message} onChange={onChangeMessage}></input>
-                        <input type="submit" value={"Send"} />
+                        <InputMessage name='message' value={message} onChange={onChangeMessage}></InputMessage>
+                        <InputSubmit type="submit" value={"Send"} />
                     </form>
                 </Grid>
                 
@@ -77,3 +104,33 @@ const Main = () => {
 }
 
 export default Main;
+
+const Exit = styled.div`
+float:right;
+`
+
+const LinkStyle = styled(Link)`
+    width: 50px;
+    height: 20px;
+    display:block;
+    margin-bottom: 10px;
+    text-align: center;
+    border: 1px solid black;
+    color:white;
+    font-size: 12px;
+    background: linear-gradient(to right, #648f61, #446142);
+    text-decoration: none;
+    &:focus, &:active,&:hover {
+        color:#0d4b9e;
+    }
+`
+
+const chatStyle = {clear:'both', height:'400px', overflowY: 'auto', border: '1px solid black', background: 'white'}
+
+const InputMessage = styled.input`
+width: 78.7%;
+`
+const InputSubmit = styled.input`
+width: 20%;
+background: linear-gradient(to right, #648f61, #446142);
+`
